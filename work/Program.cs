e42784f9ec3e75555a0186e3d41b7c9455a04058
Maria <mpriorova@mail.ru>
2023-02-16 22:40:51 +0300

    add Program.cs

---
+++ work/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array1 = new string[5] {"hello", "bay", "smile", "12", "365"};
+string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2, int num)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= num)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+const int NUMBER = 3;
+PrintArray(array1);
+Console.WriteLine();
+SecondArrayWithIF(array1, array2, NUMBER);
+PrintArray(array2);
